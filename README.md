0c4912c200e18ad09fa3c2f18dbc18fa5b090fb4
suhipek <suhipek@pm.me>
2023-09-01 11:10:20 +0800

    feat: course electing implementation

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 <div align="center">
 
-  <img src="assets/icon.png" alt="icon" width="20%">
+<img src="assets/icon.png" alt="icon" width="20%">
 
-  <h1 align="center">E站(nku eamis)助手</h1>
+<h1 align="center">E站(nku eamis)助手</h1>
 
-  滑动验证码(迫真)绕过
 
 </div>
-
 
 ## Usage
 
@@ -20,10 +18,13 @@ eamis.cookie_eamis # use this for course electing
 
 ## To-Do
 
-- [x] 绕过SSO滑动验证码(迫真)登录E站
-- [ ] 获取其他cookies参数：`LOGIN`、`SCREEN_NAME`、`semester.id`
-- [ ] 选课相关
-  - [ ] 课程列表获取
-  - [ ] 上限已选人数获取
-  - [ ] 进行一个课的选
+- [X] 绕过SSO滑动验证码(迫真)登录E站
+- [X] 获取其他cookies参数：`semester.id`
+- [X] 选课相关
+  - [X] 选课列表获取
+  - [X] 课程列表获取
+  - [X] 上限已选人数获取
+  - [X] 进行一个课的选
 - [ ] CLI
+- [ ] 计划运行器
+- [ ] Json-based Triggers & Flows
--- eamis.py
+++ eamis.py
@@ -3,9 +3,12 @@ import requests
 import hashlib
 import base64
 import time
+import json
+import re
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
 from Crypto import Random
+from bs4 import BeautifulSoup
 
 default_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
@@ -31,6 +34,7 @@ class eamis:
         self.cookie_eamis = self.prepare_eamis_cookie()
         JSESSIONID = self.cookie_eamis['JSESSIONID']
         self.cookie_sso = self.prepare_sso_cookie(JSESSIONID)
+        self.course_list_cache = {}
         
     def prepare_eamis_cookie(self):
         homeExt = requests.get(
@@ -97,5 +101,152 @@ class eamis:
             cookies=self.cookie_sso,
             allow_redirects=False, 
             verify=self.verify)
-        return rand.json()['rand']
+        return rand.json()['rand']        
     
+    def check_login(self):
+        home = requests.get(
+            'https://eamis.nankai.edu.cn/eams/home.action',
+            headers=self.default_headers, 
+            cookies=self.cookie_sso,
+            allow_redirects=False,
+            verify=self.verify
+        )
+        if home.status_code == 200:
+            return True
+        return False
+    
+    def get_all_semesters(self):
+        all_semesters = requests.get(
+            'https://eamis.nankai.edu.cn/eams/dataQuery.action?dataType=semesterCalendar', 
+            headers=self.default_headers, 
+            cookies=self.cookie_eamis, 
+            verify=self.verify)
+        corrected_json = re.sub(r'(\w+):', r'"\1":', 
+                                all_semesters.text)
+        return json.loads(corrected_json)
+    
+    def switch_to_current_semester(self):
+        timestamp = str(int(time.time() * 1000))
+        courseTableForStd = requests.get(
+            f'https://eamis.nankai.edu.cn/eams/courseTableForStd!innerIndex.action?projectId=1&_={timestamp}', 
+            headers=self.default_headers, 
+            cookies=self.cookie_eamis, 
+            verify=self.verify)
+        self.cookie_eamis.update(courseTableForStd.cookies)
+
+    def get_course_table(self, project_id):
+        timestamp = str(int(time.time() * 1000))
+        courseTableForStd = requests.get(
+            f'https://eamis.nankai.edu.cn/eams/courseTableForStd!innerIndex.action?projectId=1&_={timestamp}', 
+            headers=self.default_headers, 
+            cookies=self.cookie_eamis, 
+            verify=self.verify)
+        pattern = r'if\(jQuery\("#courseTableType"\)\.val\(\)=="std"\){\s*bg\.form\.addInput\(form,"ids","(\d+)"\);'
+        match = re.search(pattern, courseTableForStd.text)
+        ids = match.group(1)
+        
+        # TODO: prase the course table
+        return courseTableForStd
+            
+    def get_course_list(self, profile_id):
+        params = {
+            'profileId': profile_id,
+        }
+        headers = self.default_headers.copy()
+        headers['Referer'] = f'https://eamis.nankai.edu.cn/eams/stdElectCourse!defaultPage.action?electionProfile.id={profile_id}'
+        stdElectCourse = requests.get(
+            'https://eamis.nankai.edu.cn/eams/stdElectCourse!data.action',
+            params=params,
+            cookies=self.cookie_eamis,
+            headers=headers,
+            verify=self.verify
+        )
+        pattern = r'var lessonJSONs = (.*?);'
+        match = re.search(pattern, stdElectCourse.text)
+        corrected_json = re.sub(r'(\w+):', r'"\1":', 
+                        match.group(1)).replace('\'', '"')
+        return json.loads(corrected_json)
+    
+    def get_profile_list(self):
+        params = {
+            'projectId': '1',
+        }
+
+        stdElectCourse = requests.get(
+            'https://eamis.nankai.edu.cn/eams/stdElectCourse!innerIndex.action',
+            params=params,
+            cookies=self.cookie_eamis,
+            headers=self.default_headers,
+            verify=self.verify
+        )
+        soup = BeautifulSoup(stdElectCourse.text, 'html.parser')
+
+        profile_info = {}
+
+        divs = soup.find_all('div', class_='ajax_container')
+        for div in divs:
+            header = div.find('h3')
+            link = div.find('a', href=True)
+
+            if header and link:
+                title = header.get_text(strip=True)
+                url = link['href']
+                profile_id = url.split('=')[-1]
+                profile_info[profile_id] = title
+                
+        return profile_info
+    
+    def refresh_course_list(self, profile_id):
+        self.course_list_cache[profile_id] = self.get_course_list(profile_id)
+        
+    def get_student_count(self, semesterId):
+        """get student count of each course
+
+        Args:
+            semesterId (int): semester id, can use cookie_eamis['semester.id'] to get current semester id
+
+        Returns:
+            dict: /*sc 当前人数, lc 人数上限,upsc 计划外实际人数  ,uplc 计划外上限*/
+        """
+        params = {
+            'projectId': '1',
+            'semesterId': semesterId,
+        }
+
+        queryStdCount = requests.get(
+            'https://eamis.nankai.edu.cn/eams/stdElectCourse!queryStdCount.action',
+            params=params,
+            cookies=self.cookie_eamis,
+            headers=self.default_headers,
+            verify=self.verify
+        )
+        
+        pattern = r'window.lessonId2Counts=(.*?)'
+        match = re.search(pattern, queryStdCount.text)
+        corrected_json = re.sub(r'(\w+):', r'"\1":', 
+                        match.group(1)).replace('\'', '"')
+        return json.loads(corrected_json)
+    
+    def batch_operator(self, profile_id: int, course_id: int, operator: bool):
+        headers = self.default_headers.copy()
+        headers['Referer'] = 'https://eamis.nankai.edu.cn/eams/stdElectCourse!defaultPage.action'
+        params = {
+            'profileId': str(profile_id),
+        }
+        operator = 'true' if operator else 'false'
+        data = {
+            'optype': operator,
+            'operator0': f'{course_id}:{operator}:0',
+            'lesson0': f'{course_id}',
+            f'expLessonGroup_{course_id}': 'undefined',
+        }
+
+        response = requests.post(
+            'https://eamis.nankai.edu.cn/eams/stdElectCourse!batchOperator.action',
+            params=params,
+            cookies=self.cookie_eamis,
+            headers=headers,
+            data=data,
+        )
+        
+        return response
